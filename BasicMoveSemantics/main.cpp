2f7c155e2f6cef43e03655779c0c2a2510efbba7
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-10 23:13:06 -0400

    Review of some move semantics

--- BasicMoveSemantics/main.cpp
+++ BasicMoveSemantics/main.cpp
@@ -1,14 +1,36 @@
-//
-//  main.cpp
-//  BasicMoveSemantics
-//
-//  Created by Matt Ahmad on 2022-10-10.
-//
-
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+class A
+{
+public:
+    
+    A() : s("default") {}
+    
+    A(const A& o) : s(o.s) { cout << "move failed!\n";}
+    
+    A(A&& o) : s(move(o.s)) {}
+    
+    string printObj() { return s; }
+    
+private:
+    string s;
+};
+
+A temp(A a) {
+    return a;
+}
+    
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    A a1 = temp(A());
+    
+    cout << "before move() : a1 = " << a1.printObj() << endl;
+    A a2 = move(a1);
+    cout << "after move() : a1 = " << a1.printObj() << endl;
+    cout << "after move() : a2 = " << a2.printObj() << endl;
+    
     return 0;
 }
